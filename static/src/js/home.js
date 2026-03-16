a990057eed5b736101a86237207961d1648a416e
Mike Watt <watt.mike@gmail.com>
2026-03-16 16:50:46 -0500

    Add gallery section/schema

---
+++ static/src/js/home.js
@@ -0,0 +1,34 @@
+/**
+ * Home template
+ * This file contains logic specific to a template.
+ * It is intended to ONLY be loaded on templates that require
+ * this specific functionality.
+ **/
+
+import Global from "./lib/global";
+import Logger from "./lib/logger";
+
+// Vars
+// ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+const NAME = "Home";
+
+// Logic
+// ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+/**
+ * init - Initialize the page
+ **/
+const init = () => {
+	Logger.info(`${NAME} > init`);
+};
+
+// Bootstrap global functionality
+Global();
+
+// Bootstrap page-specific functionality
+init();
+
+// Do not edit below this line...
+// ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+Logger.log(`${NAME} > loaded`);
---
+++ static/src/js/lib/global.js
@@ -0,0 +1,34 @@
+/**
+ * Global Functionality
+ * The logic in this is intended to ONLY contain any functionality
+ * that is required on every page load.
+ *
+ * NOTE: Please do not include any page-specific logic here.
+ **/
+
+import Logger from "../lib/logger";
+
+export default function () {
+  const NAME = 'Global';
+  Logger.info(`${NAME} > loading`);
+
+  // Vars
+  // ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+
+  // Logic
+  // ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+
+  /**
+   * init --- Initialization block
+   */
+  function init() {
+    Logger.info(`${NAME} > init`);
+  }
+
+  // Do not edit below this line...
+  // ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒
+  Logger.info(`${NAME} > loaded`);
+
+  init();
+}
